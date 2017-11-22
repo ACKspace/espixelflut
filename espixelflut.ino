6693bffac8836ab277c604e55e36be7724d0a6df
xopr@ackspace.nl <xopr@ackspace.nl>
2017-11-22 08:01:18 +0100

    cleanup, added brightness, timeout, gamma, changed packet size

--- espixelflut.ino
+++ espixelflut.ino
@@ -1,14 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <EEPROM.h>
-#include <NeoPixelBus.h>
+//#include <NeoPixelBus.h>
+#include <NeoPixelBrightnessBus.h>
 
-//#define DEBUG
+#define DEBUG
+#define LED_DEBUG_IP
 #define UDP_PORT 1234
 #define PIN_ROTARY1 0
 #define PIN_ROTARY2 4
 #define PIN_PUSHBUTTON 5
 #define PIN_LEDS 0
+#define ESSID ""
+#define WPA_PASS ""
+#define TIMEOUT 10 * 60 * 1000 // 10 minutes
 
 // Debug logging
 #ifdef DEBUG
@@ -19,11 +24,12 @@
 
 // Create udp interface
 WiFiUDP Udp;
-char g_incomingPacket[ 255 ];
-
-NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip( 250 );      // use rx0/gpio3
+char g_incomingPacket[ 16 ];
+NeoPixelBrightnessBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip( 250 );      // use rx0/gpio3
 //NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip( 250 );     // tx1/gpio2
 //NeoPixelBus<NeoGrbFeature, NeoEsp8266AsyncUart800KbpsMethod> strip( 250 );// tx1/gpio2
+NeoGamma<NeoGammaTableMethod> colorGamma;
+
 volatile bool g_bFalling_Edge = false;
 uint8_t g_leds = 0;
 unsigned long g_time = 0;
@@ -135,11 +141,22 @@ void setup()
   }
 }
 
+
 ///////////////////////////////////////////////////////////////////////////////
 // loop
 ///////////////////////////////////////////////////////////////////////////////
 void loop()
 {
+  // TODO: Upon button press, show the IP address using the LEDs
+  
+  if ( millis() - g_time > TIMEOUT )
+  {
+    log( F( "Packet timeout: resetting colors\n" ) );
+    strip.ClearTo( RgbColor( 0, 0, 0 ) );
+    strip.Show();
+    g_time = millis();
+  }
+
   int packetSize = Udp.parsePacket();
   if (packetSize)
   {
@@ -147,37 +164,38 @@ void loop()
     Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
 #endif
 
-    int len = Udp.read( g_incomingPacket, 255 );
-    // Stringify
+    // Read packet (and leave space for a null character)
+    int len = Udp.read( g_incomingPacket, 15 );
     if ( len > 0 )
     {
+      // Stringify (null-terminate)
       g_incomingPacket[len] = 0;
 
 #ifdef DEBUG
       Serial.printf("UDP packet contents: %s\n", g_incomingPacket);
 #endif
-      
+
       byte num;
       uint32_t color;
+      RgbColor rgbColor;
       color = parseCommand( g_incomingPacket, len, num );
       if ( color != -1 )
       {
-
-          
 #ifdef DEBUG
           Serial.print( "\nLed:" );
           Serial.println( num, DEC );
           Serial.print( "Color:" );
           Serial.println( color, HEX );
 #endif
-        strip.SetPixelColor( num, RgbColor((color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff ) );
-        strip.Show(); // Initialize all pixels to 'off'
+        rgbColor = colorGamma.Correct( RgbColor((color >> 16) & 0xff, (color >> 8) & 0xff, color & 0xff ) );
+        strip.SetPixelColor( num, rgbColor );
+        strip.Show();
+        g_time = millis(); 
       }
       else
       {
         log( "failed to parse packet" );
       }
-
     }
   }  
 }
@@ -192,7 +210,7 @@ void button_ISR()
 
 bool initializeNetwork()
 {
-  WiFi.begin( "", "" );
+  WiFi.begin( ESSID, WPA_PASS );
 
   log( F( "Wifi." ) );
   while ( WiFi.status() != WL_CONNECTED )
@@ -217,6 +235,8 @@ bool initializeLeds()
   log( F( "LEDs: " ) );
   strip.Begin();
   strip.Show(); // Initialize all pixels to 'off'
+  // Set brighness to 2/3 to make sure our cables and fuse hold
+  strip.SetBrightness( 177 );
   log( F( "done\n" ) );
   return true;
 }
@@ -225,11 +245,9 @@ bool initializeEEPROM()
 {
   log( F( "EEPROM:\n" ) );
 #ifdef ESP8266
-  // Assign flash memory for EEPROM emulation
 #define E2END 511
+  // Assign flash memory for EEPROM emulation
   EEPROM.begin( E2END + 1 );
-  // Commit after write
-  //EEPROM.commit();
 #endif
   
 #ifdef DEBUG
